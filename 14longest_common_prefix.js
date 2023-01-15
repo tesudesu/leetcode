2df87df66c1c7939c542f6a0db54d45f9fd9fe44
Te Su <sute515@gmail.com>
2023-01-15 14:56:27 -0800

    Added second solution

--- 14longest_common_prefix.js
+++ 14longest_common_prefix.js
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/longest-common-prefix/
 
+// SOLUTION 1
+
 var longestCommonPrefix = function(strs) {
     let shortest = strs[0].length;
     for (let i = 1; i < strs.length; i++) {
@@ -16,4 +18,24 @@ var longestCommonPrefix = function(strs) {
         }
     }
     return result.replace(/([a-z]*)(0+)(\w*)/, '$1');
+};
+
+// SOLUTION 2
+
+var longestCommonPrefix = function(strs) {
+    let shortest = strs[0].length;
+    for (let i = 1; i < strs.length; i++) {
+        if (strs[i].length < shortest) {
+            shortest = strs[i].length;
+        }
+    }
+    let result = '';
+    for (let i = 0; i < shortest; i++) {
+        if (!strs.every((word) => word[i] === strs[0][i])) {
+            return result;
+        } else {
+            result += strs[0][i];
+        }
+    }
+    return result;
 };
