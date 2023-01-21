6b7ef448f29d3ad1aa700ef1dceb083966284b78
Te Su <sute515@gmail.com>
2023-01-20 16:52:02 -0800

    Added second solution

--- 88merge_sorted_array.js
+++ 88merge_sorted_array.js
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/merge-sorted-array/
 
+// SOLUTION 1
+
 var merge = function(nums1, m, nums2, n) {
     let counter = 0;
     for (let i = 0; i < nums1.length; i++) {
@@ -14,3 +16,13 @@ var merge = function(nums1, m, nums2, n) {
     nums1.sort((a, b) => a - b);
 };
 
+// SOLUTION 2
+
+var merge = function(nums1, m, nums2, n) {
+    let counter = 0;
+    for (let i = m; i < nums1.length; i++) {
+        nums1[i] = nums2[counter];
+        counter++;
+    }
+    nums1.sort((a, b) => a - b);
+};
