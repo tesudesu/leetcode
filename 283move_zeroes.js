0359ce4e24c2dfe98fe58c0dc5b9d7375af71415
Te Su <sute515@gmail.com>
2023-01-22 10:59:13 -0800

    updated solution

--- 283move_zeroes.js
+++ 283move_zeroes.js
@@ -1,14 +1,25 @@
 // https://leetcode.com/problems/move-zeroes/
 
 var moveZeroes = function(nums) {
-    let indZero = [];
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = nums.length - 1; i >= 0; i--) {
         if (nums[i] == 0) {
-            indZero.push(i);
+            nums.splice(i, 1);
+            nums.push(0);
         }
     }
-    for (let i = indZero.length - 1; i >= 0; i--) {
-        nums.splice(indZero[i], 1);
-        nums.push(0);
-    }
 };
+
+// Second solution
+
+// var moveZeroes = function(nums) {
+//     let indZero = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] == 0) {
+//             indZero.push(i);
+//         }
+//     }
+//     for (let i = indZero.length - 1; i >= 0; i--) {
+//         nums.splice(indZero[i], 1);
+//         nums.push(0);
+//     }
+// };
