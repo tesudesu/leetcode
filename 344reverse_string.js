f4d311edcb8835047c6eed5a1b67b840c3c82156
Te Su <sute515@gmail.com>
2023-05-06 15:18:01 -0700

    added solution

--- 344reverse_string.js
+++ 344reverse_string.js
@@ -1,9 +1,18 @@
 // https://leetcode.com/problems/reverse-string/
 
 var reverseString = function(s) {
-    const length = s.length;
-    for (let i = length - 2; i >= 0; i--) {
-        s.push(s[i]);
+    for (let i = 0; i < Math.floor(s.length/2); i++) {
+        const temp = s[i];
+        s[i] = s[s.length-1-i];
+        s[s.length-1-i] = temp;
     }
-    s.splice(0, length - 1);
+    return s;
 };
+
+// var reverseString = function(s) {
+//     const length = s.length;
+//     for (let i = length - 2; i >= 0; i--) {
+//         s.push(s[i]);
+//     }
+//     s.splice(0, length - 1);
+// };
