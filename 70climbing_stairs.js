3a1f39545ea21e9eaa5c86d742eef299a86a466a
Te Su <sute515@gmail.com>
2023-05-24 17:01:06 -0700

    edited solution

--- 70climbing_stairs.js
+++ 70climbing_stairs.js
@@ -1,14 +1,33 @@
 // https://leetcode.com/problems/climbing-stairs/
 
 var climbStairs = function(n) {
-    let stairs = [];
-    stairs[0] = 1;
-    stairs[1] = 2;
-    for (let i = 2; i <= n; i++) {
-        stairs[i] = stairs[i-1] + stairs[i-2];
+    let one = 1;
+    let two = 2;
+
+    if (n === 1) return one;
+    if (n === 2) return two;
+
+    let temp;
+
+    for (let i = 3; i <= n; i++) {
+        temp = two;
+        two = one + two;
+        one = temp;
     }
-    return stairs[n-1];
+    return two;
 };
+
+
+
+// var climbStairs = function(n) {
+//     let stairs = [];
+//     stairs[0] = 1;
+//     stairs[1] = 2;
+//     for (let i = 2; i <= n; i++) {
+//         stairs[i] = stairs[i-1] + stairs[i-2];
+//     }
+//     return stairs[n-1];
+// };
 
 // var climbStairs = function(n) {
 //     if (n === 1) return 1;
