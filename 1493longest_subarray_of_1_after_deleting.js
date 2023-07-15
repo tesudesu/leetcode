9927b8d983b3334bf881a2687a55b19823c3c4c5
Te Su <sute515@gmail.com>
2023-07-14 20:09:18 -0700

    added answer

--- 1493longest_subarray_of_1_after_deleting.js
+++ 1493longest_subarray_of_1_after_deleting.js
@@ -19,3 +19,28 @@ var longestSubarray = function(nums) {
 
     return right - left - 1;
 };
+
+// var longestSubarray = function(nums) {
+//     let left = 0;
+//     let hasZero = false;
+//     let lastZero;
+//     let longest = 0;
+
+//     for (let right = 0; right < nums.length; right++) {
+//         if (nums[right] === 0) {
+//             if (!hasZero) {
+//                 hasZero = true;
+//                 lastZero = right;
+//             } else {
+//                 longest = Math.max(longest, right - 1 - left);
+//                 left = lastZero + 1;
+//                 lastZero = right;
+//             }
+//         }
+
+//     }
+
+//     longest = Math.max(longest, nums.length - 1 - left);
+
+//     return longest;
+// };
