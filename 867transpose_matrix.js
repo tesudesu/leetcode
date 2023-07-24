9220db18facc188b50444c4c563972d7937c7816
Te Su <sute515@gmail.com>
2023-07-24 16:47:39 -0700

    added answer

--- 867transpose_matrix.js
+++ 867transpose_matrix.js
@@ -1,13 +1,25 @@
 // https://leetcode.com/problems/transpose-matrix/
 
 var transpose = function(matrix) {
-    let transposed = [];
-    for (let i = 0; i < matrix[0].length; i++) {
-        let row = [];
-        for (let j = 0; j < matrix.length; j++) {
-            row.push(matrix[j][i]);
-        }
-        transposed.push(row);
+    let ans = Array(matrix[0].length).fill(0).map(item => Array(matrix.length).fill(0));
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            ans[j][i] = matrix[i][j];
+        } 
     }
-    return transposed;
+
+    return ans;
 };
+
+// var transpose = function(matrix) {
+//     let transposed = [];
+//     for (let i = 0; i < matrix[0].length; i++) {
+//         let row = [];
+//         for (let j = 0; j < matrix.length; j++) {
+//             row.push(matrix[j][i]);
+//         }
+//         transposed.push(row);
+//     }
+//     return transposed;
+// };
