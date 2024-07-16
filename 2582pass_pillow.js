8bd22ff40057fbf19082672b9ba25895f150cb79
Te Su <sute515@gmail.com>
2024-07-16 11:32:39 -0700

    added answer

--- 2582pass_pillow.js
+++ 2582pass_pillow.js
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/pass-the-pillow/
 
 var passThePillow = function(n, time) {
+    let direction = Math.floor(time / (n - 1)) % 2;
+    let position = time % (n - 1);
+
+    if (direction === 0) {
+        return position + 1;
+    } else {
+        return n - position;
+    }
+};
+
+
+var passThePillow = function(n, time) {
     let direction = -1;
     let ind = 1;
     for (let i = 1; i <= time; i++) {
--- 2751robot_collisions.js
+++ 2751robot_collisions.js
@@ -43,3 +43,43 @@ var survivedRobotsHealths = function(positions, healths, directions) {
 
     return stack.sort((a, b) => a[3] - b[3]).map(item => item[1]);
 };
+
+
+var survivedRobotsHealths = function(positions, healths, directions) {
+    let stack = [];
+
+    let combined = positions.map((e, i) => [positions[i], healths[i], directions[i], i]);
+    combined.sort((a, b) => a[0] - b[0]);
+
+    for (let i = 0; i < combined.length; i++) {
+        let add = true;
+        while (stack.length > 0 && combined[i][2] === "L" && stack[stack.length - 1][2] === "R") {
+            if (combined[i][1] > stack[stack.length - 1][1]) {
+                stack.pop();
+                combined[i][1]--;
+            } else if (combined[i][1] === stack[stack.length - 1][1]) {
+                add = false;
+                stack.pop();
+                break;
+            } else {
+                stack[stack.length - 1][1]--;
+                add = false;
+                break;
+            }
+        }
+
+        if (add) {
+            stack.push(combined[i]);
+        }
+    }
+
+    stack.sort((a, b) => a[3] - b[3]);
+
+    let ans = [];
+
+    for (let i = 0; i < stack.length; i++) {
+        ans.push(stack[i][1]);
+    }
+
+    return ans;
+};
