645e3b994d6e4484e6d72bebb6bd800a06cd9d32
Te Su <sute515@gmail.com>
2024-09-02 21:14:27 -0700

    added answer

--- 322coin_change.js
+++ 322coin_change.js
@@ -2,7 +2,6 @@
 
 var coinChange = function(coins, amount) {
     let coinsRequired = Array(amount+1).fill(Infinity);
-    console.log(coinsRequired)
     coinsRequired[0] = 0;
     for (let i = 1; i <= amount; i++) {
         for (const coin of coins) {
@@ -12,4 +11,31 @@ var coinChange = function(coins, amount) {
         }
     }
     return coinsRequired[amount] === Infinity ? -1 : coinsRequired[amount];
+};
+
+
+var coinChange = function(coins, amount) {
+    const cache = Array(amount + 1).fill(-1);
+
+    const dp = (tot) => {
+        if (tot > amount) {
+            return Infinity;
+        }
+        if (tot === amount) {
+            return 0;
+        }
+        if (cache[tot] !== -1) {
+            return cache[tot];
+        }
+        let ans = Infinity;
+        for (let i = 0; i < coins.length; i++) {
+            ans = Math.min(ans, dp(tot + coins[i]) + 1);
+        }
+        cache[tot] = ans;
+        return ans;
+    }
+
+    const ans = dp(0);
+
+    return ans === Infinity ? -1 : ans;
 };
--- 721accounts_merge.js
+++ 721accounts_merge.js
@@ -1,5 +1,77 @@
 // https://leetcode.com/problems/accounts-merge/
 
+// Union find
+
+var accountsMerge = function(accounts) {
+    const roots = Array(accounts.length).fill();
+    const size = Array(accounts.length).fill();
+    for (let i = 0; i < accounts.length; i++) {
+        roots[i] = i;
+        size[i] = 1;
+    }
+
+    const find = (node) => {
+        if (node === roots[node]) {
+            return node;
+        }
+        // path compression
+        roots[node] = find(roots[node]);
+        return roots[node];
+    }
+
+    const union = (node1, node2) => {
+        const root1 = find(node1);
+        const root2 = find(node2);
+        if (root1 === root2) return;
+
+        // union by size
+        if (size[root1] >= size[root2]) {
+            size[root1] += size[root2];
+            roots[root2] = root1;
+        } else {
+            size[root2] += size[root1];
+            roots[root1] = root2;
+        }
+    }
+
+    const emailToIndex = new Map();
+
+    for (let i = 0; i < accounts.length; i++) {
+        for (let j = 1; j < accounts[i].length; j++) {
+            const email = accounts[i][j];
+            if (!emailToIndex.has(email)) {
+                emailToIndex.set(email, i);
+            } else {
+                union(i, emailToIndex.get(email));
+            }
+        }
+    }
+
+    const indexToEmails = new Map();
+
+    for (const [email, index] of emailToIndex) {
+        const root = find(index);
+        if (!indexToEmails.has(root)) {
+            indexToEmails.set(root, [email]);
+        } else {
+            let arr = indexToEmails.get(root);
+            arr.push(email);
+            indexToEmails.set(root, arr);
+        }
+    }
+
+    let ans = [];
+
+    for (const [index, emails] of indexToEmails) {
+        const name = accounts[index][0];
+        emails.sort();
+        ans.push([name, ...emails]);
+    }
+
+    return ans;
+};
+
+
 var accountsMerge = function(accounts) {
     let emailToIndex = {};
     let visitedAccounts = Array(accounts.length).fill(false);
