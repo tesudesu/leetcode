8e51d8f38a506eeb0e881da635f4609c2fd1b2fe
Te Su <sute515@gmail.com>
2024-12-02 20:34:48 -0800

    added answer

--- 796rotate_string.js
+++ 796rotate_string.js
@@ -1,18 +1,31 @@
 // https://leetcode.com/problems/rotate-string/
 
 var rotateString = function(s, goal) {
-    if (s == goal) {
+    if (s.length !== goal.length) return false;
+
+    s = s + s;
+
+    if (s.indexOf(goal) !== -1) {
         return true;
     }
-    let arr = s.split('');
-    for (let i = 1; i <= s.length-1; i++) {
-        const first = arr[0];
-        arr.push(first);
-        arr.shift();
-        const join = arr.join('');
-        if (join == goal) {
-            return true;
-        }
-    }
+
     return false;
 };
+
+
+// var rotateString = function(s, goal) {
+//     if (s == goal) {
+//         return true;
+//     }
+//     let arr = s.split('');
+//     for (let i = 1; i <= s.length-1; i++) {
+//         const first = arr[0];
+//         arr.push(first);
+//         arr.shift();
+//         const join = arr.join('');
+//         if (join == goal) {
+//             return true;
+//         }
+//     }
+//     return false;
+// };
