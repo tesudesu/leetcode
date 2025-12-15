02ad6e697c3d5e166b43acebbe2f77e105ff2db0
Te Su <sute515@gmail.com>
2025-12-14 21:20:13 -0800

    added answer

--- 373k_pairs_with_smallest_sums.js
+++ 373k_pairs_with_smallest_sums.js
@@ -3,30 +3,18 @@
 const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
 
 var kSmallestPairs = function(nums1, nums2, k) {
-    let minQueue = new MinPriorityQueue(a => a[0]);
-    let visited = new Set();
-    let ans = [[nums1[0], nums2[0]]];
-    if (k === 1 || (nums1.length === 1 && nums2.length === 1)) return ans;
+    let ans = [];
+    const minQueue = new MinPriorityQueue(a => a[0]);
 
-    if (nums1.length > 1) {
-        minQueue.enqueue([nums1[1] + nums2[0], 1, 0]);
-        visited.add('1,0');
-    }
-    if (nums2.length > 1) {
-        minQueue.enqueue([nums1[0] + nums2[1], 0, 1]);
-        visited.add('0,1');
+    for (let i = 0; i < nums2.length; i++) {
+        minQueue.enqueue([nums1[0] + nums2[i], 0, i]);
     }
 
-    while (k - 1 > 0 && minQueue.size() > 0) {
-        const [, x, y] = minQueue.dequeue();
-        ans.push([nums1[x], nums2[y]]);
-        if (y + 1 < nums2.length && !visited.has(`${x},${y+1}`)) {
-            minQueue.enqueue([nums1[x] + nums2[y + 1], x, y + 1]);
-            visited.add(`${x},${y+1}`);
-        }
-        if (x + 1 < nums1.length && !visited.has(`${x+1},${y}`)) {
-            minQueue.enqueue([nums1[x + 1] + nums2[y], x + 1, y]);
-            visited.add(`${x+1},${y}`);
+    while (k > 0) {
+        const [sum, i, j] = minQueue.dequeue();
+        ans.push([nums1[i], nums2[j]]);
+        if (i + 1 < nums1.length) {
+            minQueue.enqueue([nums1[i + 1] + nums2[j], i + 1, j]);
         }
         k--;
     }
